2f7903f5f421533c5e73449b5968791393d85c5e
Shengyou Fan <shengyoufan@gmail.com>
2019-11-14 17:59:58 +0800

    install Freemarker feature

--- src/Application.kt
+++ src/Application.kt
@@ -1,7 +1,10 @@
 package io.kraftsman.ktor.website
 
+import freemarker.cache.ClassTemplateLoader
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.freemarker.FreeMarker
 import io.ktor.html.respondHtml
 import io.ktor.routing.get
 import io.ktor.routing.routing
@@ -12,6 +15,10 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+    }
 
     routing {
         get("/") {
