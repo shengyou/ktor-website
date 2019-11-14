c17dcfc0965a860827a045810b45e9fadc238f8a
Shengyou Fan <shengyoufan@gmail.com>
2019-11-14 17:52:38 +0800

    respondHtml with DSL

--- src/Application.kt
+++ src/Application.kt
@@ -2,11 +2,10 @@ package io.kraftsman.ktor.website
 
 import io.ktor.application.Application
 import io.ktor.application.call
-import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode
-import io.ktor.response.respondText
+import io.ktor.html.respondHtml
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import kotlinx.html.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -16,8 +15,15 @@ fun Application.module(testing: Boolean = false) {
 
     routing {
         get("/") {
-            //language=HTML
-            call.respondText("<!doctype html>\n<html lang='zh-Hant'>\n<head>\n    <meta charset='UTF-8'>\n    <meta name='viewport'\n          content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>\n    <meta http-equiv='X-UA-Compatible' content='ie=edge'>\n    <title>Hello, world</title>\n</head>\n<body>\n    <h1>Hello, world</h1>\n    <p>a sample Ktor application</p>\n</body>\n</html>", ContentType.Text.Html, HttpStatusCode.OK)
+            call.respondHtml {
+                head {
+                    title { +"Hello, HTML DSL" }
+                }
+                body {
+                    h1 { +"Hello, HTML DSL" }
+                    p { +"a sample Ktor application" }
+                }
+            }
         }
     }
 
