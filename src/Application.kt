645d74775efde7112afe5f3815b54d044968d813
Shengyou Fan <shengyoufan@gmail.com>
2019-11-14 17:48:21 +0800

    respondText with HTML string

--- src/Application.kt
+++ src/Application.kt
@@ -16,7 +16,8 @@ fun Application.module(testing: Boolean = false) {
 
     routing {
         get("/") {
-            call.respondText("Hello, world", ContentType.Text.Plain, HttpStatusCode.OK)
+            //language=HTML
+            call.respondText("<!doctype html>\n<html lang='zh-Hant'>\n<head>\n    <meta charset='UTF-8'>\n    <meta name='viewport'\n          content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>\n    <meta http-equiv='X-UA-Compatible' content='ie=edge'>\n    <title>Hello, world</title>\n</head>\n<body>\n    <h1>Hello, world</h1>\n    <p>a sample Ktor application</p>\n</body>\n</html>", ContentType.Text.Html, HttpStatusCode.OK)
         }
     }
 
