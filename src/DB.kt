86bfb46c157c204180644e8f382459218b5309f4
Shengyou Fan <shengyoufan@gmail.com>
2019-11-14 18:34:31 +0800

    seed DB

--- src/DB.kt
+++ src/DB.kt
@@ -1,9 +1,11 @@
 package io.kraftsman.ktor.website
 
+import io.kraftsman.ktor.website.entities.Task
 import io.kraftsman.ktor.website.tables.Tasks
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
 
 fun main() {
     Database.connect(
@@ -16,5 +18,16 @@ fun main() {
     transaction {
         SchemaUtils.drop(Tasks)
         SchemaUtils.create(Tasks)
+    }
+
+    transaction {
+        for (i in 1..10) {
+            Task.new {
+                title = "Task $i"
+                completed = listOf(true, false, false).shuffled().first()
+                createdAt = DateTime.now()
+                updatedAt = DateTime.now()
+            }
+        }
     }
 }
